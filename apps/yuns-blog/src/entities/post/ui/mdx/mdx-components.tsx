a0a64ab57f3a365bb5eab43638487f10b337be7a
황상윤 <s_infinite@naver.com>
2025-07-08 23:54:48 +0900

    [yuns-blog:style] mdx a, img 스타일 수정

--- apps/yuns-blog/src/entities/post/ui/mdx/mdx-components.tsx
+++ apps/yuns-blog/src/entities/post/ui/mdx/mdx-components.tsx
@@ -1,7 +1,59 @@
-import { MdxImage } from "./mdx-image";
-import { MdxLink } from "./mdx-link";
+import type { MDXComponents as tmdxComponents } from "mdx/types";
+import Image from "next/image";
+import Link from "next/link";
+import type {
+	AnchorHTMLAttributes,
+	HTMLAttributes,
+	HTMLProps,
+	ReactNode,
+} from "react";
+
+type AnchorProps = AnchorHTMLAttributes<HTMLAnchorElement>;
+// type HeadingProps = HTMLAttributes<HTMLHeadingElement>;
+// type ParagraphProps = HTMLAttributes<HTMLParagraphElement>;
+// type ElementProps = HTMLAttributes<HTMLElement>;
 
 export const MDXComponents = {
-	img: MdxImage,
-	a: MdxLink,
+	img: (props: HTMLProps<HTMLImageElement>) => {
+		return (
+			<span className="my-[16px] items-center relative w-full h-256 md:h-512">
+				<Image
+					src={props.src ?? ""}
+					alt={props.alt ?? ""}
+					title={props.alt}
+					aria-label={props.alt || ""}
+					crossOrigin="anonymous"
+					fill={true}
+					objectFit="contain"
+					className=" transition-all duration-200 rounded-md hover:opacity-70"
+				/>
+			</span>
+		);
+	},
+	a: ({ className, href, ...props }: AnchorProps) => {
+		if (!href) return null;
+		const isAnchorLink = href.startsWith("#");
+
+		if (isAnchorLink) {
+			return (
+				<a
+					aria-label={props["aria-label"]}
+					title={href.replace("#", "")}
+					href={href}
+					{...props}
+				/>
+			);
+		}
+
+		return (
+			<Link
+				className={
+					" underline underline-offset-4 text-primary-600 hover:rounded-sm duration-200 transition-colors hover:text-primary-700 hover:bg-neutral-200"
+				}
+				target={"_blank"}
+				href={href ?? ""}
+				{...props}
+			/>
+		);
+	},
 };
